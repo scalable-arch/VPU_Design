e148e4c3c70b1e77c92533f5b19baa892b7e405b
HanumKo <990520kog2@gmail.com>
2024-10-05 22:24:20 +0900

    GoldenModel_update

--- sw/C/Goldenmodel/test_vector/test_vector_only_positive.py
+++ sw/C/Goldenmodel/test_vector/test_vector_only_positive.py
@@ -1,8 +1,16 @@
 import random
 
-# BF16 데이터를 생성하는 함수 (0x0000부터 0xFFFF까지의 16진수 값)
+# BF16 데이터를 생성하는 함수 (NaN과 Infinity를 제외한 값)
 def generate_bf16_data():
-    return random.randint(0x0000, 0xFFFF)
+    # 부호 비트 (0 또는 1)
+    sign = 0
+    # 지수부 (0x00부터 0xFE까지, 0xFF 제외)
+    exponent = random.randint(0x01, 0x7E)
+    # 가수부 (0x00부터 0x7F까지)
+    significand = random.randint(0x01, 0x7F)
+    # BF16 값 생성
+    bf16_value = (sign << 15) | (exponent << 7) | significand
+    return bf16_value
 
 # BF16 데이터를 96개 생성하고 파일에 쓰는 함수
 def create_bf16_file(filename, num_lines=100):
@@ -12,4 +20,4 @@ def create_bf16_file(filename, num_lines=100):
             file.write(" ".join(bf16_data) + "\n")  # 한 줄에 96개 데이터를 공백으로 구분하여 작성
 
 # 파일 생성
-create_bf16_file("bf16_numbers.txt")
+create_bf16_file("bf16_numbers_only_positive.txt")
